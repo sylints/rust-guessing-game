5c1cb50063958b1ef59a5f29b50d510f7176286b
Herby <sylints@gmail.com>
2018-04-06 22:47:54 -0600

    full program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate rand;
+
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the guessing game!");
+    loop {
+        let secret_number = rand::thread_rng().gen_range(1, 101);
+        println!("The secret number is {}", secret_number);
+
+        println!("Enter your number: ");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You guessed it!");
+                break;
+            }
+        }
+    }
 }
